032a8cdff3ee5279898af68ffa6e35596ef437a9
lazyboismile <lazyboijon@gmail.com>
2025-11-12 00:41:13 +0500

    feat: install LightRays components & update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 const page = () => {
   return (
-    <h1>Welcome to Next16!</h1>
+    <section>
+      <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Palace</p>
+    </section>
   )
 }
 

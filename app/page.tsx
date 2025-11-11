c77ea3261528bd0821062d49dff41928511753af
lazyboismile <lazyboijon@gmail.com>
2025-11-12 01:49:30 +0500

    feat: create Navbar & EventCard and generate ai based data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { events } from "@/lib/constants"
+import EventCard from "./components/EventCard"
 import ExploreButton from "./components/ExploreButton"
 
 const page = () => {
@@ -7,6 +9,18 @@ const page = () => {
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Palace</p>
 
       <ExploreButton />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event}/>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }

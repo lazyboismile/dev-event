a2fe0a8e71a9d3e3bda5f02bd4f3856c9ae72c3c
lazyboismile <lazyboijon@gmail.com>
2025-11-12 00:53:21 +0500

    feat: create ExploreButton component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import ExploreButton from "./components/ExploreButton"
+
 const page = () => {
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Palace</p>
+
+      <ExploreButton />
     </section>
   )
 }
